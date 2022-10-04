f8507270da4ee3a8ae37604cdb0bff4050e7a5db
Orozco Valdez Florencio Miguel <72531146+FlorencioOrozco@users.noreply.github.com>
2022-10-04 13:32:55 -0500

    Add files via upload

---
+++ Parcial1/ConfigurandoReact/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Orozco Valdez Florencio Miguel 7°E </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
